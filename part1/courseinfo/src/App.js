b39332d1ae8ab9e9abf0274a945438d5a028fb31
mtisby <marieltisby1998@gmail.com>
2021-08-31 17:39:29 -0700

    homework

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -31,36 +31,34 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        total: {props.parts[0].exercises + props.parts[1].exercise + props.parts[2].exercises}
+        total: {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}
       </p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }]
-  
+  const course ={
+    name :'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }]
+  }
   return (
     <div>
-      <Header course={course} />
-
-      <Part parts={parts} />
-      
-      <Total parts={parts} />
-      
+      <Header course={course.name} />
+      <Part parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
